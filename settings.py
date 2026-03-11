44a4361a2a94698bf96692bb5109f93c5de9993c
Egypt Soldier <russianboy43214@gmail.com>
2026-03-12 00:41:49 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.zilantsoftwaresystem.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "mailer@zilantsoftwaresystem.com"
+EMAIL_HOST_PASSWORD = "P@ssw0rdMailer2025.@"
+
+DEFAULT_FROM_EMAIL = "Ipek M. <mailer@zilantsoftwaresystem.com>"
+SERVER_EMAIL = "server@zilantsoftwaresystem.com"
